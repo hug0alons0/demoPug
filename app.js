cde9ba5be565f6c0bcb608ceaab8a8ee2a0b1500
hug0alons0 <82771725+hug0alons0@users.noreply.github.com>
2021-05-31 16:15:38 +0200

    addedImg

--- app.js
+++ app.js
@@ -3,10 +3,15 @@ const router = require('./controllers/index')
 const app = express()
 const port = 3000
 
+
 app.set('view engine', 'pug');
 app.set('views','./views');
 
+app.use(express.static('public'));
+
 app.use('/', router)
+
+
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
